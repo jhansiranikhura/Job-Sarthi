d3926d70af80d16a97ea183b789e72aad1f824be
Jhansi <123853656+jhansiranikhura@users.noreply.github.com>
2025-12-21 08:19:59 +0530

    update company.route.js implementation

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -5,7 +5,7 @@ import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-
+router.route("/register").post(isAuthenticated,registerCompany);
 router.route("/get").get(isAuthenticated,getCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
 router.route("/update/:id").patch(isAuthenticated,singleUpload, updateCompany);
